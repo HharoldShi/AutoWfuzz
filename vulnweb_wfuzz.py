73445e17214c70a28000c3952e8bae02b7eeaabe
Harold <j85shi@uwaterloo.ca>
2020-08-17 18:11:19 -0400

    add function: clustering fuzz results; add wrapper file

--- vulnweb_wfuzz.py
+++ vulnweb_wfuzz.py
@@ -1,6 +1,51 @@
 import wfuzz
 from bs4 import BeautifulSoup
+import sys
 
+
+class FuzzResultEntry:
+    def __init__(self, code, lines, words, chars, url, error_msg):
+        self.code = code
+        self.lines = lines
+        self.words = words
+        self.chars = chars
+        self.url = url
+        self.error_msg = error_msg
+
+    def __eq__(self, other):
+        if isinstance(other, FuzzResultEntry):
+            return self.code == other.code and self.lines==other.lines and self.words==other.words and self.chars==other.chars
+        return False
+
+    def __hash__(self):
+        return hash((self.code, self.lines, self.words, self.chars))
+
+
+class FuzzResultClusters:
+    def __init__(self):
+        self.cluster_list = []
+
+    def append_entry(self, r):
+        if len(r.plugins['errors']) != 0:
+            entry = FuzzResultEntry(r.code, r.lines, r.words, r.chars, r.url, error_msg=r.plugins['errors'][0])
+        else:
+            entry = FuzzResultEntry(r.code, r.lines, r.words, r.chars, r.url, None)
+        if entry not in self.cluster_list or entry.error_msg is not None:
+            self.cluster_list.append(entry)
+
+    def output(self):
+        sys.stdout.write("=======================================================================================================================================\n")
+        sys.stdout.write("ClusterID      Response     Lines      Words     Chars      Error                                                                   URL\n")
+        sys.stdout.write("=======================================================================================================================================\n")
+        clusterid = 1
+        for entry in self.cluster_list:
+            sys.stdout.write("{0:9d}\t   {1:3d}\t    {2} L      {3}       {4}       {5}\t    {6}\t    \n".format(
+                clusterid, entry.code, entry.lines, entry.words, entry.chars, str(entry.error_msg), entry.url))
+            clusterid += 1
+
+
+# Gloabl varaibles
+fuzz_result_cluters = FuzzResultClusters()
 home_url = "http://testphp.vulnweb.com"
 
 
@@ -16,21 +61,27 @@ def find_parameters():
     return
 
 
+
 def fuzz_sql_injection(url, parameter):
-    with wfuzz.get_session("-z file,./wordlist/Injections/SQL.txt {0}?{1}=FUZZ".format(url, parameter)) as s:
+    sys.stdout.write("\nWFuzz -- SQL Injection\n")
+    with wfuzz.get_session("-z file,./wfuzz-master/wordlist/Injections/SQL.txt -A {0}?{1}=FUZZ".format(url, parameter)) as s:
         for r in s.fuzz():
-            print(r)
+            fuzz_result_cluters.append_entry(r)
+        fuzz_result_cluters.output()
+
 
 
 def fuzz_xss_injection(url, parameter):
-    with wfuzz.get_session("-z file,./wordlist/Injections/XSS.txt {0}?{1}=FUZZ -A".format(url, parameter)) as s:
+    sys.stdout.write("\nWFuzz -- XSS Injection\n")
+    with wfuzz.get_session("-z file,./wfuzz-master/wordlist/Injections/XSS.txt -A -Z {0}?{1}=FUZZ".format(url, parameter)) as s:
         for r in s.fuzz():
-            print(r)
+            fuzz_result_cluters.append_entry(r)
+        fuzz_result_cluters.output()
 
 
 def main():
-    fuzz_sql_injection(home_url+"/search.php",'test')
-
+    fuzz_sql_injection(home_url + "/search.php", 'test')
+    fuzz_xss_injection(home_url + "/search.php", 'test')
 
 
 
