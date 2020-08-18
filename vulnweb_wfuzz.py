62ca18f5d3c43f0c15c20894feffefd243e139ee
Xinyi Ma <34392418+XinyiM@users.noreply.github.com>
2020-08-17 22:51:08 -0400

    Update vulnweb_wfuzz.py

--- vulnweb_wfuzz.py
+++ vulnweb_wfuzz.py
@@ -2,7 +2,6 @@ import wfuzz
 from bs4 import BeautifulSoup
 import sys
 
-
 class FuzzResultEntry:
     def __init__(self, code, lines, words, chars, url, error_msg):
         self.code = code
@@ -35,34 +34,16 @@ class FuzzResultClusters:
 
     def output(self):
         sys.stdout.write("=======================================================================================================================================\n")
-        sys.stdout.write("ClusterID      Response     Lines      Words     Chars      Error                                                                   URL\n")
+        sys.stdout.write("ClusterID      Response     Lines      \tWords     \tChars      \tError                                                         URL\n")
         sys.stdout.write("=======================================================================================================================================\n")
         clusterid = 1
         for entry in self.cluster_list:
-            sys.stdout.write("{0:9d}\t   {1:3d}\t    {2} L      {3}       {4}       {5}\t    {6}\t    \n".format(
+            sys.stdout.write("{0:9d}\t   {1:3d}\t\t    {2} L\t      {3}\t       {4}\t      {5}\t    {6}\t    \n".format(
                 clusterid, entry.code, entry.lines, entry.words, entry.chars, str(entry.error_msg), entry.url))
             clusterid += 1
 
 
-# Gloabl varaibles
-fuzz_result_cluters = FuzzResultClusters()
-home_url = "http://testphp.vulnweb.com"
-
-
-def find_links():
-    fh = open('Home of Acunetix Art.html')
-    soup = BeautifulSoup(fh, 'html.parser')
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-    return
-
-
-def find_parameters():
-    return
-
-
-
-def fuzz_sql_injection(url, parameter):
+def fuzz_sql_injection(url, parameter, fuzz_result_cluters):
     sys.stdout.write("\nWFuzz -- SQL Injection\n")
     with wfuzz.get_session("-z file,./wfuzz-master/wordlist/Injections/SQL.txt -A {0}?{1}=FUZZ".format(url, parameter)) as s:
         for r in s.fuzz():
@@ -70,8 +51,7 @@ def fuzz_sql_injection(url, parameter):
         fuzz_result_cluters.output()
 
 
-
-def fuzz_xss_injection(url, parameter):
+def fuzz_xss_injection(url, parameter, fuzz_result_cluters):
     sys.stdout.write("\nWFuzz -- XSS Injection\n")
     with wfuzz.get_session("-z file,./wfuzz-master/wordlist/Injections/XSS.txt -A -Z {0}?{1}=FUZZ".format(url, parameter)) as s:
         for r in s.fuzz():
@@ -79,44 +59,34 @@ def fuzz_xss_injection(url, parameter):
         fuzz_result_cluters.output()
 
 
-def main():
-    fuzz_sql_injection(home_url + "/search.php", 'test')
-    fuzz_xss_injection(home_url + "/search.php", 'test')
 
 
 
 
 
 
+# for r in wfuzz.fuzz(url="http://testphp.vulnweb.com/FUZZ", hc=[404], payloads=[("file",dict(fn="wordlist/general/common.txt"))]):
+#     print(r)
+#     print(r.history.cookies.response)
+#     print(r.history.params.all)
 
+# with wfuzz.get_session("-z list --zD test -u http://testphp.vulnweb.com/userinfo.php -d uname=FUZZ&pass=FUZZ") as s:
+#     for r in s.fuzz():
+#         print(r.history.cookies.response)
+#         print(r.history.params.all)
+#         print(r.history.params.post)
+#         print(r.history.params.post.uname)
+#         print(r.history.params.post['pass'])
 
+# with wfuzz.get_session("-z file,./wordlist/general/common.txt -u http://testphp.vulnweb.com/FUZZ") as s:
+#     for r in s.fuzz():
+#         print(r)
+        # print(r.history.cookies)
+        # print(r.history.params.location)
+        # if r.code == 301:
+        #     print(r.history.headers.response.Location)
 
-
-    # for r in wfuzz.fuzz(url="http://testphp.vulnweb.com/FUZZ", hc=[404], payloads=[("file",dict(fn="wordlist/general/common.txt"))]):
-    #     print(r)
-    #     print(r.history.cookies.response)
-    #     print(r.history.params.all)
-
-    # with wfuzz.get_session("-z list --zD test -u http://testphp.vulnweb.com/userinfo.php -d uname=FUZZ&pass=FUZZ") as s:
-    #     for r in s.fuzz():
-    #         print(r.history.cookies.response)
-    #         print(r.history.params.all)
-    #         print(r.history.params.post)
-    #         print(r.history.params.post.uname)
-    #         print(r.history.params.post['pass'])
-
-    # with wfuzz.get_session("-z file,./wordlist/general/common.txt -u http://testphp.vulnweb.com/FUZZ") as s:
-    #     for r in s.fuzz():
-    #         print(r)
-            # print(r.history.cookies)
-            # print(r.history.params.location)
-            # if r.code == 301:
-            #     print(r.history.headers.response.Location)
-
-    # with wfuzz.get_session("--script=robots -z list,'robots.txt' http://testphp.vulnweb.com/FUZZ") as s:
-    #     for r in s.fuzz():
-    #         print(r)
-
-if __name__ == '__main__':
-    main()
+# with wfuzz.get_session("--script=robots -z list,'robots.txt' http://testphp.vulnweb.com/FUZZ") as s:
+#     for r in s.fuzz():
+#         print(r)
 
